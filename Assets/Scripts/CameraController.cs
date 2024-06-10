1bcb614535a47ba8550d77d96cb35420c41224d3
Elumenix <dps5393@g.rit.edu>
2024-06-10 15:59:03 -0400

    Got Bezier transition mostly working
    need to find a way to make rotation feel cleaner

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,11 +6,19 @@ using UnityEngine.EventSystems;
 
 public class CameraController : MonoBehaviour
 {
+    public GameObject p1Test;
+    public GameObject p2Test;
+    
     public GravitationalBody target; // the object to follow
     public GravityManager planetManager;
     public GameObject circlePrefab;
     private List<ValueTuple<GameObject, float>> circles; // Value 1: Planet, Value 2: Progress in Lerp function
     public GameObject orientationModel;
+
+    private Transform startBezierTransform;
+    private Transform endBezierTransform;
+    private bool followingBezier;
+    private float lerpAmount;
     
     // Controls how responsive mouse rotation is
     public float distance = 10.0f; // distance from target
@@ -20,7 +28,7 @@ public class CameraController : MonoBehaviour
     private bool mouseDown; // Tracked so that first clicking on screen doesn't teleport camera
     private bool dragging; // Tracked so that releasing mouse after drag doesn't suddenly teleport to a planet
     private int clickTimer = 15;
-    private bool followPlanetRotation;
+    public bool followPlanetRotation;
 
     // Variables that track circle Ui changes
     private int lastIndexHovered;
@@ -31,8 +39,11 @@ public class CameraController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        followPlanetRotation = true;
-        
+        // Unity won't let me instantiate transforms on their own so this is my workaround for that
+        // It doesn't use much more memory and I won't ever create new game objects for these variables so it's alright
+        startBezierTransform = new GameObject().transform;
+        endBezierTransform = new GameObject().transform;
+
         _camera = Camera.main;
         
         // Circles are scaled down a lot to work well in scene, they only change this much
@@ -147,120 +158,141 @@ public class CameraController : MonoBehaviour
             #endregion
         }
 
-        // Camera direction and zoom will be modified based on user controls
-        #region CameraOrientationUpdate
-
-        // Variables are established here to be incremented throughout this section 
-        float deltaX = 0; // Assign pitch (side-to-side) to x
-        float deltaY = 0; // Assign yaw (up-and-down) to y
-        float deltaZ = 0; // Assign roll (forward-and-backward) to z
-        
-        // Clicking while over Ui shouldn't affect scene
-        if (!overUI)
+        // User controls are taken away during camera transition, and are instead handled by FollowBezierCurve
+        // This is done in fixedUpdate instead of update because it isn't visually related and the math
+        // To follow the Bézier curve needs to much more precise and related to the current physics for large time steps
+        // Input will not be accepted during this step so that things don't get messed up
+        if (!followingBezier)
         {
-            // Go to different Planet : only if not dragging or click completes within 15 frames of press
-            if (Input.GetMouseButtonUp(0) && lastIndexHovered != -1 && (!dragging || clickTimer > 0))
-            {
-                // Switch tracked planet and scale movement values relative to it's size
-                target = planetManager.planets[lastIndexHovered];
-                distance = target.transform.localScale.x * 10f;
-                scrollSpeed = distance / 4;
-                
-                // Position camera relative to the new target planet
-                Vector3 pos = target.transform.rotation * new Vector3(0.0f, 0.0f, -distance) + target.transform.position;
-                transform.position = pos;
-                transform.rotation = target.transform.rotation;
-            }
-        }
+            // Camera direction and zoom will be modified based on user controls, as long as not in transition
+            #region CameraOrientationUpdate
 
-        // Scroll Wheel affects distance to the planet
-        if (Input.mouseScrollDelta.y != 0 && mouseDown)
-        {
-            distance -= Input.mouseScrollDelta.y * scrollSpeed;
+            // Variables are established here to be incremented throughout this section 
+            float deltaX = 0; // Assign pitch (side-to-side) to x
+            float deltaY = 0; // Assign yaw (up-and-down) to y
+            float deltaZ = 0; // Assign roll (forward-and-backward) to z
 
-            if (distance < target.transform.localScale.x * 2)
+            // Clicking while over Ui shouldn't affect scene
+            if (!overUI)
             {
-                distance = target.transform.localScale.x * 2;
-            }
-        } 
-        
-        if (Input.GetMouseButton(0))
-        {
-            clickTimer--; // Decrement
-            
-            // Prevents teleportation of camera on the first frame you click on the window
-            if (mouseDown)
-            {
-                deltaX += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
-                deltaY -= (Input.GetAxis("Mouse Y") * ySpeed * 0.02f);
-
-                // Mouse is being used for navigation, not clicking on a planet
-                if (!Mathf.Approximately(0, deltaX) || !Mathf.Approximately(0, deltaX))
+                // Go to different Planet : only if not dragging or click completes within 15 frames of press
+                if (Input.GetMouseButtonUp(0) && lastIndexHovered != -1 && (!dragging || clickTimer > 0))
                 {
-                    dragging = true;
+                    // This will be constant so save this now. Need copies instead of references as the camera moves
+                    startBezierTransform.position = transform.position;
+                    startBezierTransform.rotation = transform.rotation;
+                    followingBezier = true; // Will start this process next frame
+                    lerpAmount = 0;
+
+                    // Switch tracked planet and scale movement values relative to it's size
+                    target = planetManager.planets[lastIndexHovered];
+                    distance = target.transform.localScale.x * 10f;
+                    scrollSpeed = distance / 4;
+
+                    // Position camera relative to the new target planet
+                    Vector3 pos = target.transform.rotation * new Vector3(0.0f, 0.0f, -distance) +
+                                  target.transform.position;
+                    transform.position = pos;
+                    transform.rotation = target.transform.rotation;
+                }
+            }
+
+            // Scroll Wheel affects distance to the planet
+            if (Input.mouseScrollDelta.y != 0 && mouseDown)
+            {
+                distance -= Input.mouseScrollDelta.y * scrollSpeed;
+
+                if (distance < target.transform.localScale.x * 2)
+                {
+                    distance = target.transform.localScale.x * 2;
+                }
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                clickTimer--; // Decrement
+
+                // Prevents teleportation of camera on the first frame you click on the window
+                if (mouseDown)
+                {
+                    deltaX += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+                    deltaY -= (Input.GetAxis("Mouse Y") * ySpeed * 0.02f);
+
+                    // Mouse is being used for navigation, not clicking on a planet
+                    if (!Mathf.Approximately(0, deltaX) || !Mathf.Approximately(0, deltaX))
+                    {
+                        dragging = true;
+                    }
+                }
+                else
+                {
+                    mouseDown = true;
                 }
             }
             else
             {
-                mouseDown = true;
+                clickTimer = 15;
+                dragging = false;
             }
+
+            // Using right click will allow movement along the third axis
+            if (Input.GetMouseButton(1))
+            {
+                if (mouseDown)
+                {
+                    float middleScreen = Screen.width / 2.0f;
+                    float middleScreenY = Screen.height / 2.0f;
+
+                    // Direction of axis change should be dependent on the side of the screen that the mouse is on
+                    if (mousePosition.x < middleScreen)
+                    {
+                        deltaZ += Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+                    }
+                    else if (mousePosition.x > middleScreen)
+                    {
+                        deltaZ -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+                    }
+
+                    if (mousePosition.y > middleScreenY)
+                    {
+                        deltaZ += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+                    }
+                    else if (mousePosition.y < middleScreenY)
+                    {
+                        deltaZ -= Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+                    }
+                }
+                else
+                {
+                    mouseDown = true;
+                }
+            }
+
+
+            // Update position of camera, required every frame since planets are moving
+            // Z Quaternion is added separately so that it doesn't affect the x/y mouse direction
+            Quaternion rotationXY = Quaternion.Euler(deltaY, deltaX, 0);
+            Quaternion rotationZ = Quaternion.Euler(0, 0, deltaZ);
+            Quaternion
+                rotation = transform.rotation * rotationZ *
+                           rotationXY; // Correct multiplication order to make x/y consistent
+
+            // Update position of camera accordingly
+            Vector3 position = rotation * new Vector3(0.0f, 0.0f, -distance) + target.transform.position;
+
+            // Make sure everything fits the new orientation
+            transform.rotation = rotation;
+            transform.position = position;
+            orientationModel.transform.rotation = rotation;
+
+            #endregion
         }
         else
         {
-            clickTimer = 15;
-            dragging = false;
+            // This still needs to update
+            orientationModel.transform.rotation = transform.rotation;
         }
-        
-        // Using right click will allow movement along the third axis
-        if (Input.GetMouseButton(1))
-        {
-            if (mouseDown)
-            {
-                float middleScreen = Screen.width / 2.0f;
-                float middleScreenY = Screen.height / 2.0f;
 
-                // Direction of axis change should be dependent on the side of the screen that the mouse is on
-                if (mousePosition.x < middleScreen)
-                {
-                    deltaZ += Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
-                }
-                else if (mousePosition.x > middleScreen)
-                {
-                    deltaZ -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
-                }
-                
-                if (mousePosition.y > middleScreenY)
-                {
-                    deltaZ += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
-                }
-                else if (mousePosition.y < middleScreenY)
-                {
-                    deltaZ -= Input.GetAxis("Mouse X") * xSpeed * 0.02f;
-                }
-            }
-            else
-            {
-                mouseDown = true;
-            }
-        }
-        
-        
-        // Update position of camera, required every frame since planets are moving
-        // Z Quaternion is added separately so that it doesn't affect the x/y mouse direction
-        Quaternion rotationXY = Quaternion.Euler(deltaY, deltaX, 0);
-        Quaternion rotationZ = Quaternion.Euler(0, 0, deltaZ);
-        Quaternion rotation = transform.rotation * rotationZ * rotationXY; // Correct multiplication order to make x/y consistent
-        
-        // Update position of camera accordingly
-        Vector3 position = rotation * new Vector3(0.0f, 0.0f, -distance) + target.transform.position;
-
-        // Make sure everything fits the new orientation
-        transform.rotation = rotation;
-        transform.position = position;
-        orientationModel.transform.rotation = rotation;
-
-        #endregion
-        
         // Updates the Ui circles for each planet to be between the camera and planet at the appropriate size
         #region CirclePositioning&Visibility
         
@@ -282,7 +314,7 @@ public class CameraController : MonoBehaviour
                         LayerMask.GetMask("Sun"); // Only Sun
 
                     // Essentially, hide circle if the planet is obscured by the sun, (and/or earth/moon in moons case)
-                    if (Physics.Linecast(position, planet.transform.position, layerMask))
+                    if (Physics.Linecast(transform.position, planet.transform.position, layerMask))
                     {
                         // Will be invisible, continue
                         circles[i].Item1.SetActive(false);
@@ -306,7 +338,7 @@ public class CameraController : MonoBehaviour
                     continue;
             }
 
-            Vector3 dir = planet.transform.position - position;
+            Vector3 dir = planet.transform.position - transform.position;
             float dist = dir.magnitude;
             float tarDist = (target.transform.position - _camera!.transform.position).magnitude;
             
@@ -323,7 +355,7 @@ public class CameraController : MonoBehaviour
 
                 // Put circles the same distance from the camera as the planet, and scale them to maintain the same size
                 // The goal of this is so that the planet covers the ui for other planets that would be hidden behind it
-                Vector3 newPosition = position + dir.normalized * tarDist;
+                Vector3 newPosition = transform.position + dir.normalized * tarDist;
                 
                 circles[i].Item1.transform.position = newPosition;
 
@@ -349,16 +381,70 @@ public class CameraController : MonoBehaviour
 
     public void FixedUpdate()
     {
-        if (followPlanetRotation && Time.timeScale != 0)
+        if (followPlanetRotation && Time.timeScale != 0 && !followingBezier)
         {
             // Reverse Direction if time is reversed
             int timeDir = !planetManager.reversed ? 1 : -1;
             
             // This is a series of vector/quaternion transformations to make the camera rotate around the planet
-            // at teh exact same speed that the planet is rotating, making it look still
+            // at the exact same speed that the planet is rotating, making it look still
             float deltaRotation = (float) (target.RotationSpeed * timeDir * Time.fixedDeltaTime);
             Vector3 rotationAxis = transform.InverseTransformDirection(target.transform.up); // Aligns camera to planet
             transform.Rotate(rotationAxis, deltaRotation, Space.Self); // transformation relative to camera
         }
+
+        if (followingBezier)
+        {
+            // 3 seconds should pass for transition to finish
+            lerpAmount += Time.fixedDeltaTime / 20;
+            
+            // Position and rotation of end planet changes every update, so it keeps needing to be recalculated
+            // Note that we aren't calculating the planets transform, but instead the camera's end goal
+            // This is much more important for the situation where we're following the planets rotations as it 
+            // Always have the camera end on the same face of the planet regardless of start position/rotation
+            endBezierTransform = target.transform; // Gets rotation correct immediately
+            endBezierTransform.position = endBezierTransform.rotation * new Vector3(0.0f, 0.0f, -distance) +
+                                          target.transform.position;
+            
+            FollowBezierCurve(startBezierTransform, endBezierTransform, lerpAmount > 1 ? 1 : lerpAmount);
+            
+            // Transition has finished
+            if (lerpAmount >= 1)
+            {
+                followingBezier = false;
+            }
+        }
+    }
+
+    // This function isn't meant to move the camera along a Bézier curve to transition between planets
+    private void FollowBezierCurve(Transform start, Transform end, float t)
+    {
+        // Because I want to create a cubic Bézier curve, and this isn't an animation system, I need 4 control points
+        // Because I only start with two, I need to mathematically create two more control points to use
+        // this should be mathematically reasonable so that the camera doesn't move all over the place
+        
+        // Calculate direction away from planets that control points should be
+        // Because all planets hover around z = 0, I want the direction to meaningfully deviate on that axis
+        Vector3 direction = (end.position - start.position);
+        direction = new Vector3(direction.x, direction.y, direction.magnitude / 3).normalized;
+
+        // How far away I want the control point to be from others
+        float controlPointDistance = Vector3.Distance(start.position, end.position) / 3f; 
+
+        Vector3 controlPoint1 = start.position + direction * controlPointDistance;
+        Vector3 controlPoint2 = end.position - direction * controlPointDistance;
+
+        //p1Test.transform.position = controlPoint1;
+        //p2Test.transform.position = controlPoint2;
+        
+        // We follow De Casteljau's Algorithm for following the Bézier curve
+        Vector3 A = Vector3.Lerp(start.position, controlPoint1, t);
+        Vector3 B = Vector3.Lerp(controlPoint1, controlPoint2, t);
+        Vector3 C = Vector3.Lerp(controlPoint2, endBezierTransform.position, t);
+        Vector3 D = Vector3.Lerp(A, B, t);
+        Vector3 E = Vector3.Lerp(B, C, t);
+
+        transform.position = Vector3.Lerp(D, E, t);
+        transform.rotation = Quaternion.Lerp(startBezierTransform.rotation, endBezierTransform.rotation, t);
     }
 }
